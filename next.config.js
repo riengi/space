e3dd6b292ed7c0b14075e2b317922ae9cf40a7a3
riengi <112477297+riengi@users.noreply.github.com>
2022-10-16 11:09:50 +0200

    Next.js update and styled component config added

--- next.config.js
+++ next.config.js
@@ -4,4 +4,9 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    styledComponents: true
+  }
+}
